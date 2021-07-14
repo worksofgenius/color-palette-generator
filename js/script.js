4ca710e8bfe111ea54000f3ce329bd9919a6aceb
Marisa Cassidy <marisancassidy@gmail.com>
2021-07-14 21:57:07 +0900

    Add rgb values

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ let html = '';
 const randomValue = () => Math.floor(Math.random() * 256);
 
 function randomRGB(value) {
-  const color = `rgb( ${value()}, ${value()}, ${value()} )`;
+  const color = `rgb(${value()}, ${value()}, ${value()})`;
   return color;
 }
 
 for ( let i = 1; i <= 10; i++ ) {
-  html += `<div style="background-color: ${randomRGB(randomValue)}">${i}</div>`;
+  html += `<div style="background-color: ${randomRGB(randomValue)}">${randomRGB(randomValue)}</div>`;
 }
 
 document.querySelector('main').innerHTML = html;
